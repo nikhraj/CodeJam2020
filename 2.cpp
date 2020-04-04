f5bc40385abd30ac3cba2eef27e768ae1df2cd05
nikhraj <55176863+nikhraj@users.noreply.github.com>
2020-04-04 23:11:00 +0530

    Create 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+        string a;
+        cin >> a;
+        vector<int> vc;
+        int n = a.length();
+        for (int i = 0; i < n; i++)
+        {
+            vc.push_back(a[i] - '0');
+        }
+        cout << "Case #" << test << ": ";
+        int cc = vc[0];
+        while (cc--)
+        {
+            cout << '(';
+        }
+        cout << vc[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (vc[i] - vc[i - 1] > 0)
+            {
+                int c = abs(vc[i] - vc[i - 1]);
+                while (c--)
+                {
+                    cout << '(';
+                }
+                cout << vc[i];
+            }
+            else if (vc[i] - vc[i - 1] == 0)
+            {
+                cout << vc[i];
+            }
+            else
+            {
+                int c = abs(vc[i] - vc[i - 1]);
+                while (c--)
+                {
+                   cout << ')';
+                }
+                cout << vc[i];
+            }
+        }
+        while (vc[n - 1]--)
+        {
+           cout << ')';
+        }
+
+        cout << " " << endl;
+
+}
+
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
